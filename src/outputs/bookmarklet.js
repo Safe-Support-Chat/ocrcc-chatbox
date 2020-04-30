bc68a4028ce31698dc8b7349ddad159145f5cc53
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-04-30 10:12:36 -0400

    update config

--- src/outputs/bookmarklet.js
+++ src/outputs/bookmarklet.js
@@ -1,17 +1,17 @@
 import EmbeddableChatbox from './embeddable-chatbox';
 
 var config = {
-  matrixServerUrl: 'https://matrix.rhok.space',
-  botId: '@help-bot:rhok.space',
-  roomName: 'Support Chat',
-  termsUrl: 'https://tosdr.org/',
-  introMessage: 'This chat application does not collect any of your personal data or any data from your use of this service.',
-  agreementMessage: 'Do you want to continue?',
-  confirmationMessage: 'Waiting for a facilitator to join the chat...',
-  exitMessage: 'The chat is closed. You may close this window.',
-  chatUnavailableMessage: 'The chat service is not available right now. Please try again later.',
-  anonymousDisplayName: 'Anonymous',
-}
+        matrixServerUrl: 'https://matrix.safesupport.chat',
+        botId: '@help-bot:safesupport.chat',
+        roomName: 'Support Chat',
+        termsUrl: 'https://tosdr.org/',
+        introMessage: "This is just a demo! There is probably no-one at the other end. You should probaby email us instead. But if you're lucky, there might be someone online to say hi!",
+        agreementMessage: 'Do you want to continue?',
+        confirmationMessage: 'Waiting for a facilitator to join the chat...',
+        exitMessage: 'The chat is closed. You may close this window.',
+        chatUnavailableMessage: 'The chat service is not available right now. Please try again later.',
+        anonymousDisplayName: 'Anonymous',
+    }
 
 export default function bookmarklet() {
   if (window.EmbeddableChatbox) {
