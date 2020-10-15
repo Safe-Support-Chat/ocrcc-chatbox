7bd6cf2466e84be115361d28d9d680830d753fe9
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-10-15 12:24:59 -0400

    package component

--- webpack.config.js
+++ webpack.config.js
@@ -105,4 +105,18 @@ module.exports = [{
     libraryExport: 'default',
     libraryTarget: 'commonjs2',
   },
+  externals: {
+    react: {
+        commonjs: "react",
+        commonjs2: "react",
+        amd: "React",
+        root: "React"
+    },
+    "react-dom": {
+        commonjs: "react-dom",
+        commonjs2: "react-dom",
+        amd: "ReactDOM",
+        root: "ReactDOM"
+    }
+  }
 }];
